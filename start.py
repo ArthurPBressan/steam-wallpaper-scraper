9608c66447791c41e40c1a05e4b39d106c7b2f8a
Arthur Bressan <arthurpbressan@gmail.com>
2018-01-28 11:39:32 -0200

    Força logout se acontecer algum problema. closes #5

--- start.py
+++ start.py
@@ -3,23 +3,37 @@ from steam.enums import EResult
 
 import scraper
 
-client = SteamClient()
 
-print("One-off login recipe")
-print("-"*20)
+def _user_aborted():
+    print()
+    print('Aborted by user')
 
-result = client.cli_login()
 
-if result != EResult.OK:
-    print("Failed to login: %s" % repr(result))
-    raise SystemExit
+def main():
+    client = SteamClient()
 
-print("-"*20)
-print("Logged on as:", client.user.name)
-print("Community profile:", client.steam_id.community_url)
-print("Last logon:", client.user.last_logon)
-print("Last logoff:", client.user.last_logoff)
+    print("Steam Cards Wallpaper Downloader")
+    print("-"*20)
 
-scraper.scrape(client)
+    try:
+        result = client.cli_login()
+    except (EOFError, KeyboardInterrupt):
+        _user_aborted()
+        raise SystemExit
 
-client.logout()
+    if result != EResult.OK:
+        print("Failed to login: %s" % repr(result))
+        raise SystemExit
+    print("-"*20)
+    print("Logged on as:", client.user.name)
+
+    try:
+        scraper.scrape(client)
+    except KeyboardInterrupt:
+        _user_aborted()
+    finally:
+        client.logout()
+
+
+if __name__ == '__main__':
+    main()
